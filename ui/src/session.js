1b3976e97df7898a239a881f4022fd0fc372b8a8
Azeem Bande-Ali <me@azeemba.com>
2023-12-24 13:08:04 -0500

    Add zooming/panning on detailed images

--- ui/src/session.js
+++ ui/src/session.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from "react-router-dom";
 import { SessionDetailCard } from './sessionDetailCard';
+import { Carousel } from 'flowbite-react';
 import 'tailwindcss/tailwind.css'
 
 export async function loader({ params }) {
@@ -10,18 +11,30 @@ export async function loader({ params }) {
 }
 
 function Session() {
+    const [modalPreviewDetails, requestdModalPreview] = useState({});
     const details = useLoaderData();
     const timestamps = Object.keys(details)
 
+    function handleTriggerModalPreview({ key, targetUrl }) {
+        requestdModalPreview({
+            timestamp: key,
+            targetUrl: targetUrl
+        })
+    }
     const cards = timestamps.map(ts => (
-        <SessionDetailCard key={ts} session={details[ts]} />
+        <SessionDetailCard key={ts}
+            session={details[ts]}
+            timestamp={ts}
+            modalPreview={modalPreviewDetails.timestamp === ts ? modalPreviewDetails : null}
+            triggerModalPreview={handleTriggerModalPreview}
+        />
     ))
     return (
-    <div className="bg-gray-900 min-h-screen text-white flex justify-center">
-        <div className="container flex flex-wrap">
-        {cards}
+        <div className="bg-gray-900 min-h-screen text-white flex justify-center">
+            <Carousel pauseOnHover slide={!modalPreviewDetails.timestamp}>
+                {cards}
+            </Carousel>
         </div>
-    </div>
     )
 }
 
--- ui/src/sessionDetailCard.js
+++ ui/src/sessionDetailCard.js
@@ -1,38 +1,77 @@
+import { TransformWrapper, TransformComponent, MiniMap } from "react-zoom-pan-pinch";
 import { useInView } from 'react-intersection-observer';
+import { Modal, Button } from "flowbite-react";
+import { Fragment } from "react";
 
-export function SessionDetailCard({ session }) {
+export function SessionDetailCard({ session, timestamp, modalPreview, triggerModalPreview }) {
 
-const { ref, inView } = useInView({
-    triggerOnce: true,  // image will load once and won't unload
-});
+    const { ref, inView } = useInView({
+        triggerOnce: true,  // image will load once and won't unload
+    });
 
-let border = "divide-y-2"
-let processesTable = session.processes.map((proc, index) => (
+    let border = "divide-y-2"
+    let processesTable = session.processes.map((proc, index) => (
         <tr key={index} className={`${proc.active ? "bg-sky-800" : ""} ${border}`}>
             <td>{proc.process}</td>
             <td>{proc.title}</td>
         </tr>
     ));
 
-return (
-    <div className="p-4 bg-gray-900 rounded shadow-md flex flex-col md:flex-row text-white" ref={ref}>
-       <div className="flex-1">
-            {inView ? <img  src={session.image} alt="Session Detail"/> : null}
-        </div>
-        <div className="flex-1 p-4">
-            <h2>{session.display_time}</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Process</th>
-                        <th>Title</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {processesTable}
-                </tbody>
-            </table>
+    function launchImage(p) {
+        console.log(session)
+        let data = { key: timestamp, targetUrl: p.target.src }
+        console.log("Image clicked", data)
+        triggerModalPreview(data)
+    }
+
+    return (
+        <div className="p-4 bg-gray-900 rounded shadow-md flex flex-col text-white" ref={ref}>
+            <Modal size="7xl" dismissible show={modalPreview != null} onClose={() => triggerModalPreview({})}>
+                <Modal.Body >
+                    <div className="h-[80vh]">
+                        <TransformWrapper initialScale={2} centerOnInit={true}>
+
+                            {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                                <Fragment>
+                                    <div style={{
+                                        position: "fixed",
+                                        zIndex: 5,
+                                        top: "75px",
+                                        right: "50px",
+                                    }}>
+                                        <MiniMap width={100}><img src={modalPreview && modalPreview.targetUrl} alt="Minimap" /> </MiniMap>
+                                    </div>
+                                    <TransformComponent wrapperStyle={{ maxWidth: "100%", maxHeight: "calc(70vh)", width: "100%", height: "100%" }}>
+                                        <img src={modalPreview && modalPreview.targetUrl} alt="A zoomable version" />
+                                    </TransformComponent>
+                                    <div className="flex flex-row space-x-3">
+                                        <Button onClick={() => resetTransform()}>Reset</Button>
+                                        <Button onClick={() => triggerModalPreview({})}>Close</Button>
+                                    </div>
+
+                                </Fragment>
+                            )}
+                        </TransformWrapper>
+                    </div>
+                </Modal.Body>
+            </Modal>
+            <div className="flex-1">
+                {inView ? (<img src={session.image} alt="Session Detail" onClick={launchImage} />) : null}
             </div>
-    </div>
-)
+            <div className="flex-1 p-4">
+                <h2>{session.display_time}</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Process</th>
+                            <th>Title</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {processesTable}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
 }
