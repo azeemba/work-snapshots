bc711d01dde9633366817d9ddd8f9b873dc7eaae
Azeem Bande-Ali <me@azeemba.com>
2023-12-26 22:57:37 -0500

    Add tagging and editing title ability

--- ui/src/WorkSessionSummaries.js
+++ ui/src/WorkSessionSummaries.js
@@ -1,20 +1,84 @@
 import React, { useEffect, useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import { Button, TextInput } from "flowbite-react";
 
 import SessionCard from "./sessionCard";
 
 export default function WorkSessionsSummary() {
   const [sessions, setSessions] = useState([]);
+  const [availableTags, setAvailableTags] = useState([]);
+
+  const [shouldShowAddTags, setShowAddTags] = useState(false);
+  const [newTagValue, setNewTagValue] = useState("");
+
   useEffect(() => {
     fetch("/api/worksessions")
       .then((response) => response.json())
       .then((data) => setSessions(data));
+    fetch("/api/tags")
+      .then((response) => response.json())
+      .then((data) => setAvailableTags(data.tags));
   }, []);
+
+  const handleEdit = ({ id, title, tag }) => {
+    fetch(`/api/worksessions/${id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: title,
+        tag: tag,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((resp) => {
+        setSessions(resp);
+      });
+  };
+
+  const addNewTag = (ev) => {
+    fetch(`/api/tags/`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        tag: newTagValue,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((resp) => setAvailableTags(resp.tags))
+      .then(() => {
+        setShowAddTags(false);
+      });
+  };
 
   return (
     <div className="container mx-auto px-2 md:px-0 py-5">
+      <div className="h-10 my-2">
+        {shouldShowAddTags ? (
+          <div className="flex flex-row">
+            <TextInput
+              defaultValue={newTagValue}
+              onInput={(e) => setNewTagValue(e.target.value)}
+            ></TextInput>
+            <Button onClick={addNewTag}>Add Tag</Button>
+          </div>
+        ) : (
+          <Button onClick={(e) => setShowAddTags(true)}>
+            <FaPlus />
+          </Button>
+        )}
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {sessions.map((session, i) => (
-          <SessionCard key={i} session={session} />
+          <SessionCard
+            key={i}
+            session={session}
+            availableTags={availableTags}
+            onEdit={handleEdit}
+          />
         ))}
       </div>
     </div>
--- ui/src/sessionCard.js
+++ ui/src/sessionCard.js
@@ -4,28 +4,36 @@ import "tailwindcss/tailwind.css";
 import { FaCheck, FaPencilAlt, FaPlus } from "react-icons/fa";
 import { FaXmark } from "react-icons/fa6";
 import { Link } from "react-router-dom";
-import { Button, Badge } from "flowbite-react";
+import { Button, Badge, TextInput } from "flowbite-react";
 
-function SessionCard({ session }) {
+function SessionCard({ session, availableTags, onEdit }) {
   const [inEditMode, setEditMode] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(session.title);
+  const [editedTag, setEditedTag] = useState(session.tag);
   const { ref, inView } = useInView({
     triggerOnce: true,
   });
 
   // Get available tags?
-  const availableTags = [{ name: "Ump Show" }, { name: "WorkSnapshots" }]
   const tagDropdown = (
     <div className="flex flex-row h-8">
       <select
         id="countries"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2"
-        onClick={ev => ev.preventDefault()}
+        onClick={(e) => e.preventDefault()}
+        onChange={(e) => setEditedTag(e.target.value)}
       >
-        {availableTags.map((t) => <option key={t.name}>{t.name}</option>)}
+        <option key="empty" value="">
+          None
+        </option>
+        {availableTags.map((t) => (
+          <option key={t.tag} value={t.tag}>
+            {t.tag}
+          </option>
+        ))}
       </select>
     </div>
-  )
-
+  );
 
   const startDate = session.display_time;
   let duration = `${session.duration_minutes} minutes`;
@@ -57,12 +65,14 @@ function SessionCard({ session }) {
   };
   const saveButtonClicked = (ev) => {
     ev.preventDefault();
+    console.log("Will add override", editedTitle, editedTag);
     setEditMode(false);
-  }
+    onEdit({ id: session.id, title: editedTitle, tag: editedTag });
+  };
   const noSaveClicked = (ev) => {
     ev.preventDefault();
     setEditMode(false);
-  }
+  };
 
   return (
     <Link to={link}>
@@ -77,43 +87,49 @@ function SessionCard({ session }) {
           <img className="w-full" src={session.image} alt="Session Preview" />
         ) : null}
         <div className="p-4">
-          <h2 className="font-bold text-xl mb-1 text-white">{session.title}</h2>
+          {inEditMode ? (
+            <TextInput
+              onClick={(e) => e.preventDefault()}
+              value={editedTitle}
+              onInput={(e) => setEditedTitle(e.target.value)}
+            ></TextInput>
+          ) : (
+            <h2 className="font-bold text-xl mb-1 text-white">
+              {session.title}
+            </h2>
+          )}
           <p className="text-md text-gray-300 mb-1">{startDate}</p>
           <p className="text-sm text-gray-400">Duration: {duration}</p>
           <div className="flex flex-row justify-between py-2">
-            {inEditMode ? tagDropdown :
+            {inEditMode ? (
+              tagDropdown
+            ) : (
               <div className="h-8">
-                {session.tag ?
-                  <Badge>session.tag.name</Badge> : <Badge color="gray">Untagged</Badge>
-                }
+                {session.tag ? (
+                  <Badge>{session.tag}</Badge>
+                ) : (
+                  <Badge color="gray">Untagged</Badge>
+                )}
               </div>
-            }
-            {
-              inEditMode ?
-                <div className="flex flex-row gap-4">
-                  <Button
-                    size="sm"
-                    color="green"
-                    onClick={saveButtonClicked} >
-                    <FaCheck />
-                  </Button>
-                  <Button
-                    size="sm"
-                    color="red"
-                    onClick={noSaveClicked} >
-                    <FaXmark />
-                  </Button>
-                </div>
-                :
-
-                <Button
-                  size="sm"
-                  className="bg-indigo-800 text-indigo-50 transition-all enabled:hover:bg-indigo-500"
-                  onClick={editButtonClicked}
-                >
-                  <FaPencilAlt />
+            )}
+            {inEditMode ? (
+              <div className="flex flex-row gap-4">
+                <Button size="sm" color="green" onClick={saveButtonClicked}>
+                  <FaCheck />
                 </Button>
-            }
+                <Button size="sm" color="red" onClick={noSaveClicked}>
+                  <FaXmark />
+                </Button>
+              </div>
+            ) : (
+              <Button
+                size="sm"
+                className="bg-indigo-800 text-indigo-50 transition-all enabled:hover:bg-indigo-500"
+                onClick={editButtonClicked}
+              >
+                <FaPencilAlt />
+              </Button>
+            )}
           </div>
         </div>
       </div>
