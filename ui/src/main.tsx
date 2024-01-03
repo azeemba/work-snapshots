d69cf8a6c8a3ca0c3665ca3cbc4b260200e204f1
Azeem Bande-Ali <me@azeemba.com>
2024-01-02 21:51:45 -0500

    Add a tags page to give cumulative stats
    Can add more stuff in the future but this is basic stuff to get started with. Gives aggregate hours by tags

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -6,18 +6,26 @@ import "./index.css";
 import WorkSession, { loader as sessionLoader } from "./pages/worksession.tsx";
 import WorkSessionsSummaries from "./pages/worksessionsummaries.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import TagsPage from "./pages/tags.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { path: "/", element: <WorkSessionsSummaries /> },
+      { 
+        path: "/",
+        element: <WorkSessionsSummaries />
+      },
       {
         path: "/session/:sessionId",
         element: <WorkSession />,
         loader: sessionLoader,
       },
+      {
+        path: "/stats",
+        element: <TagsPage/>
+      }
     ],
   },
 ]);
---
+++ ui/src/pages/tags.tsx
@@ -0,0 +1,51 @@
+import { useOutletContext } from "react-router-dom";
+import { OutletContextInfo } from "../App";
+import { displayMinutes } from "../util/time";
+import TagBadge from "../components/tagbadge";
+
+export default function TagsPage() {
+  // Figure out how to get the data
+  const {allSessions, availableTags} = useOutletContext<OutletContextInfo>();
+  const aggregateDurationByTags = () => {
+    const durations: { [key: string]: number } = {};
+    for (const session of allSessions) {
+      if (session.tag in durations) {
+        durations[session.tag] += session.duration_minutes;
+      } else {
+        durations[session.tag] = session.duration_minutes;
+      }
+    }
+    return durations;
+  };
+  const durations = aggregateDurationByTags();
+  const durationrows = [];
+  const durationPairs = []
+  for (const [key, duration] of Object.entries(durations)) {
+    durationPairs.push({key, duration});
+  }
+  durationPairs.sort((a, b) => b.duration - a.duration);
+
+  for (const {key, duration} of durationPairs)
+  {
+    durationrows.push(
+      <div key={key} className="flex flex-row flex-wrap gap-2 w-full max-w-lg justify-between pt-4">
+        <TagBadge tag={ key } availableTags={availableTags} />
+        <div>{displayMinutes(duration)}</div>
+      </div>
+    );
+  }
+
+  // What we want to show?
+
+  // Time spent on each tag in total
+  // Time spent on each tag in time period
+  // Time spent changing over time 
+
+  return (
+    <div className="container mx-auto px-2 py-5">
+      <div className="flex flex-col content-center gap-3 divide-y">
+      {durationrows}
+    </div>
+  </div>
+  );
+}
